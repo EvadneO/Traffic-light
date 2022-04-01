5dbe329cac1de7073a3632f79705ebfb9557aa4a
EvadneO <evadne28@gmail.com>
2022-04-01 22:04:55 +0000

    semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,19 +6,56 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="rojo">
+				<a
+					href="#"
+					className="btn btn-danger"
+					onClick={() =>
+						function setColor(evento) {
+							const [red, setColorRed] = useState("");
+							if (evento.target.classList.contains("red")) {
+								setColorRed("selected");
+							}
+							<div
+								className={"red circulo " + red}
+								onClick={setColor}></div>;
+						}
+					}></a>
+			</div>
+			<div className="amarillo">
+				<a
+					href="#"
+					className="btn btn-warning"
+					onClick={() =>
+						function setColor(evento) {
+							const [yellow, setColorYellow] = useState("");
+							if (evento.target.classList.contains("yellow")) {
+								setColorYellow("selected");
+							}
+							<div
+								className={"yellow circulo " + yellow}
+								onClick={setColor}></div>;
+						}
+					}></a>
+			</div>
+			<div className="verde">
+				<a
+					href="#"
+					className="btn btn-success"
+					onClick={() =>
+						function setColor(evento) {
+							const [green, setColorGreen] = useState("");
+							if (evento.target.classList.contains("green")) {
+								setColorGreen("selected");
+							}
+							<div
+								className={"green circulo " + green}
+								onClick={setColor}></div>;
+						}
+					}></a>
+			</div>
+			<div className="footer"></div>
 		</div>
 	);
 };
